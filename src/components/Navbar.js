46d2b72726f2ce7b954cfb2fd58eb03e38521701
Jay Farahani <jayfarahani@Jays-iMac.home>
2022-10-27 07:56:14 -0500

    Updated nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <div
-            className="container-fluid py-3 mb-4"
-            style={{ backgroundColor: "slateGray" }}
-        >
-            <div className="row">
-                <div className="col-12">
-                    <h4 className="p-0 m-0 text-white">RP ROOMSHOTS</h4>
+        <div className="" style={{ backgroundColor: "slateGray" }}>
+            <div className="container-fluid container-lg py-3 mb-4">
+                <div className="row">
+                    <div className="col-12">
+                        <h4 className="p-0 m-0 text-white">
+                            <Link
+                                to="/rp-roomshot-primary/"
+                                className="text-white text-decoration-none"
+                            >
+                                RP ROOMSHOTS
+                            </Link>
+                        </h4>
+                    </div>
                 </div>
             </div>
         </div>
